e12efdef60ea592393ff53db27199e65b6c497a3
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2023-12-28 11:02:17 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number1 = scanner.nextInt();
+        double number2 = scanner.nextInt();
+        System.out.println(number1 / number2);
+    }
+}
