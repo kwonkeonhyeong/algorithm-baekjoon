ea42b7416daeea087c63fca66968d81dee3591bb
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2024-01-10 23:43:02 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1008 ms, Memory: 288080 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String testCount = br.readLine();
+        int count = Integer.parseInt(testCount);
+
+        for (int i = 0; i < count; i++) {
+            String input = br.readLine();
+            String[] splitInput = input.split(" ");
+            int a = Integer.parseInt(splitInput[0]);
+            int b = Integer.parseInt(splitInput[1]);
+
+            bw.write(a+b+"\n");
+        }
+        bw.flush();
+    }
+}
