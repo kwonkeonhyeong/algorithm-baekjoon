f811cf1da2808eae06773c7a6e36c784bee28461
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2024-04-03 17:41:11 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int V = scanner.nextInt();
+
+        double result = (double)(V-B)/(A-B);
+        System.out.println((int) Math.ceil(result));
+    }
+}
